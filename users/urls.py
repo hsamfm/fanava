97f7e871ee96e4026391082da282f83a89721e68
hesam <hsamfm>
2019-07-06 17:56:36 +0430

    login and register added

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path('login/', views.LoginView.as_view(), name='users-login'),
+    path('login/', LoginView.as_view(), name='users-login'),
     path('register/', RegisterView.as_view(), name='users-register'),
     path('forgetpassword/', views.PasswordResetView.as_view(), name='users-forgetpassword'),
     path('admin/panel/', AdminView.as_view(), name='users-panel'),
--- users/views.py
+++ users/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.views.generic import View
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout, login, authenticate
+from django.contrib.auth.hashers import check_password, make_password
 from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+# from .forms import UserProfileInfoForm, UserForm
+from django.contrib.auth.forms import UserCreationForm
+from django.forms import ValidationError
+from django.utils.translation import gettext, gettext_lazy as _
+from django.contrib import messages as message
 
 
 # Create your views here.
@@ -14,28 +22,48 @@ class LoginView(View):
         return render(request, self.template_name, context={})
 
     def post(self, request, *args, **kwargs):
-        pass
+        if request.method == "POST":
+            email = request.POST['email']
+            password = request.POST['password']
+            this_user = get_object_or_404(User, email=email)
+            if check_password(password, this_user.password):
+                message.info(request, "با موفقیت وارد شدید !")
+                return redirect(f'/users/admin/panel/?status=true&email={email}')
+            else:
+                info = message.info(request, "با موفقیت وارد شدید !")
+                errors = message.error(request, "ایمیل یا رمز عبور اشتباه است‌‌ !")
+                return render(request, self.template_name,
+                              context={'message_errors': errors, 'message_success': info})
 
 
 class RegisterView(View):
     template_name = 'admin/pages/examples/register.html'
 
+    error_messages = {
+        'password_mismatch': _("پسورد ها باهم برابر نیستند !"),
+    }
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name, context={})
 
     def post(self, request, *args, **kwargs):
-        if request.user.email.endswith('@gmail.com'):
-            return redirect('/users/register/')
-        else:
-            if request.method == "POST":
-                username = request.POST['username']
-                email = request.POST['email']
-                password = request.POST['password']
-                password_conf = request.POST['password_conf']
+        if request.method == "POST":
+            username = request.POST['username']
+            email = request.POST['email']
+            password1 = request.POST['password']
+            password2 = request.POST['password_conf']
 
-                register = get_object_or_404(User, username, email, password, password_conf)
-                if register is not User.objects.exists():
-                    pass
+            if password1 and password2 and password1 != password2:
+                raise ValidationError(self.error_messages["password_mismatch"], code='password_mismatch')
+            elif User.objects.filter(username=request.POST['username'], email=request.POST['email']).exists():
+                return HttpResponse("Login Field!")
+            else:
+                u = User.objects.create_user(username, email, password1)
+                u.save()
+                return render(request, self.template_name,
+                              context={"message": "با موفقیت ثبت نام شدید لطفا لاگین کنید !"})
+
+        return render(request, self.template_name, context={'errors': "مثل اینکه قبلا ثبت نام کرده اید !"})
 
 
 class ForgetPassword(View):
@@ -51,9 +79,9 @@ class ForgetPassword(View):
 class AdminView(View):
     template_name = 'admin/index.html'
 
-    @login_required(login_url='/users/login/')
+    # @login_required(login_url='/users/login/')
     def get(self, request):
-        pass
+        return render(request, self.template_name, context={})
 
     def post(self, request, *args, **kwargs):
         pass
