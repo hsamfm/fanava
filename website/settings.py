684ea5b818270ee4ed7407737d1e6cefa432bf73
hsamfm <40150209+hsamfm@users.noreply.github.com>
2019-07-06 18:06:34 +0430

    Update settings.py

--- website/settings.py
+++ website/settings.py
@@ -110,23 +110,14 @@ WSGI_APPLICATION = 'website.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 DATABASES = {
     'default': {
-        "ENGINE": 'django.db.backends.mysql',
-        "NAME": 'webapp',
-        "HOST": '127.0.0.1',
-        "PORT": '3306',
-        "USER": 'root',
-        "PASSWORD": ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
@@ -172,53 +163,3 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Disable Django's logging setup
-LOGGING_CONFIG = None
-
-LOGLEVEL = os.environ.get('LOGLEVEL', 'info').upper()
-
-logging.config.dictConfig({
-    'version': 1,
-    'disable_existing_loggers': False,
-    'formatters': {
-        'default': {
-            # exact format is not important, this is the minimum information
-            'format': '%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-        },
-        'django.server': DEFAULT_LOGGING['formatters']['django.server'],
-    },
-    'handlers': {
-        # console logs to stderr
-        'console': {
-            'class': 'logging.StreamHandler',
-            'formatter': 'default',
-        },
-        # Add Handler for Sentry for `warning` and above
-        'sentry': {
-            'level': 'WARNING',
-            'class': 'raven.contrib.django.raven_compat.handlers.SentryHandler',
-        },
-        'django.server': DEFAULT_LOGGING['handlers']['django.server'],
-    },
-    'loggers': {
-        # default for all undefined Python modules
-        '': {
-            'level': 'WARNING',
-            'handlers': ['console', 'sentry'],
-        },
-        # Our application code
-        'app': {
-            'level': LOGLEVEL,
-            'handlers': ['console', 'sentry'],
-            # Avoid double logging because of root logger
-            'propagate': False,
-        },
-        # Prevent noisy modules from logging to Sentry
-        'noisy_module': {
-            'level': 'ERROR',
-            'handlers': ['console'],
-            'propagate': False,
-        },
-        # Default runserver request logging
-        'django.server': DEFAULT_LOGGING['loggers']['django.server'],
-    },
-})
